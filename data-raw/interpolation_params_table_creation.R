434f6c7b99d29e37df3a0dad4b3a484039cfb5b1
MalditoBarbudo <victorgrandagarcia@gmail.com>
2020-06-09 15:47:06 +0200

    interpolation params table creation

---
+++ data-raw/interpolation_params_table_creation.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(stringr)
+library(purrr)
+
+# files
+interpolator_files <- list.files(
+  'data-raw/interpolation_objects', '_calibrated.rds', full.names = TRUE
+)
+
+# function to use in map, it loads the file and extract the parameters, buidling
+# a tibble with an extra column indicating the year
+interpolation_params_extractor <- function(file) {
+  interpolator <- readRDS(file)
+  dplyr::as_tibble(interpolator@params) %>%
+    mutate(year = stringr::str_extract(file, pattern = "[0-9]+"))
+}
+
+# map_dfr
+interpolation_params_table <-
+  interpolator_files %>%
+  purrr::map_dfr(interpolation_params_extractor) %>%
+  dplyr::arrange(year)
+
+db_conn <- pool::dbPool(
+  RPostgres::Postgres(),
+  dbname = 'meteoland', host = 'laboratoriforestal.creaf.uab.cat', port = 5432,
+  password = rstudioapi::askForPassword(), user = 'ifn'
+)
+
+dplyr::copy_to(
+  db_conn, interpolation_params_table, 'interpolation_parameters',
+  overwrite = TRUE, temp = FALSE
+)
+
+pool::poolClose(db_conn)
+
+
+
+
+
+
