dfac6fbdddcbf1d00e5d8c85096178007db7e25b
MalditoBarbudo <victorgrandagarcia@gmail.com>
2020-07-02 13:29:42 +0200

    started mainDataOutput

---
+++ R/mod_mainDataOutput.R
@@ -0,0 +1,107 @@
+#' @title mod_mainDataOutput and mod_mainData
+#'
+#' @description Shiny module to get the data
+#'
+#' @param id
+#'
+#' @export
+mod_mainDataOutput <- function(id) {
+  ns <- shiny::NS(id)
+  return()
+}
+
+#' @title mod_mainData server function
+#'
+#' @param input internal
+#' @param output internal
+#' @param session internal
+#'
+#' @param data_reactives,map_reactives reactives from modules
+#' @param meteolanddb object to access the nfi db
+#' @param lang lang selected
+#' @param parent_session parent session to be able to update tabset panel
+#'
+#' @importFrom dplyr n
+#'
+#' @export
+#'
+#' @rdname mod_mainDataOuput
+mod_mainData <- function(
+  input, output, session,
+  data_reactives, map_reactives,
+  meteolanddb, lang, parent_session
+) {
+
+  # custom polygon ####
+  # we need to check if custom polygon, to retrieve it and build the data later
+  custom_polygon <- shiny::reactive({
+
+    shiny::validate(
+      shiny::need(data_reactives$data_type, 'no inputs yet')
+    )
+
+    data_type <- data_reactives$data_type
+    path_to_file <- data_reactives$user_file_sel$datapath
+
+    # file
+    if (data_type == 'file') {
+      # check if there is user file
+      if (is.null(path_to_file)) {
+        user_file_polygons <- NULL
+      } else {
+        # check if zip (shapefile) or gpkg to load the data
+        if (stringr::str_detect(path_to_file, 'zip')) {
+          tmp_folder <- tempdir()
+          utils::unzip(path_to_file, exdir = tmp_folder)
+
+          user_file_polygons <- sf::st_read(
+            list.files(tmp_folder, '.shp', recursive = TRUE, full.names = TRUE),
+            as_tibble = TRUE
+          ) %>%
+            sf::st_transform(4326)
+        } else {
+          # gpkg
+          user_file_polygons <- sf::st_read(path_to_file) %>%
+            sf::st_transform(4326)
+        }
+      }
+
+      shiny::validate(
+        shiny::need(user_file_polygons, 'no file provided')
+      )
+
+      # rename the poly_id
+      names(user_file_polygons)[1] <- 'poly_id'
+
+      return(user_file_polygons)
+    }
+
+    if (data_type == 'drawn_poly') {
+      # validation
+      drawn_polygon <- map_reactives$nfi_map_draw_all_features
+      # When removing the features (custom polygon) the
+      # input$map_draw_new_feature is not cleared, so is always filtering the
+      # sites, even after removing. For that we need to control when the removed
+      # feature equals the new, that's it, when we removed the last one
+      shiny::validate(
+        shiny::need(drawn_polygon, 'no draw polys yet'),
+        shiny::need(length(drawn_polygon[['features']]) != 0, 'removed poly')
+      )
+      res <-
+        drawn_polygon[['features']][[1]][['geometry']][['coordinates']] %>%
+        purrr::flatten() %>%
+        purrr::modify_depth(1, purrr::set_names, nm = c('long', 'lat')) %>%
+        dplyr::bind_rows() %>%
+        {list(as.matrix(.))} %>%
+        sf::st_polygon() %>%
+        sf::st_sfc() %>%
+        sf::st_sf(crs = 4326) %>%
+        dplyr::mutate(poly_id = 'drawn_poly')
+      return(res)
+    }
+  })
+
+
+
+
+}
