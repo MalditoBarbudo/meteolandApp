2da92928ad5170f84232b73760a82649b69b1cc1
MalditoBarbudo <victorgrandagarcia@gmail.com>
2021-11-25 12:42:57 +0100

    security changes2

--- data-raw/daily_historic_tables_creation.R
+++ data-raw/daily_historic_tables_creation.R
@@ -11,7 +11,7 @@ nc_files <- list.files(
 db_conn <- pool::dbPool(
   RPostgres::Postgres(),
   dbname = 'meteoland', host = 'laboratoriforestal.creaf.cat', port = 5432,
-  password = 'rstudioapi::askForPassword('ifn')creaf', user = 'ifn'
+  password = rstudioapi::askForPassword('ifn'), user = 'ifn'
 )
 db_checkout <- pool::poolCheckout(db_conn)
 
@@ -58,7 +58,7 @@ for (file_year in nc_files) {
       db_conn <- pool::dbPool(
         RPostgres::Postgres(),
         dbname = 'meteoland', host = 'laboratoriforestal.creaf.cat', port = 5432,
-        password = 'rstudioapi::askForPassword('ifn')creaf', user = 'ifn'
+        password = rstudioapi::askForPassword('ifn'), user = 'ifn'
       )
       db_checkout <- pool::poolCheckout(db_conn)
 
